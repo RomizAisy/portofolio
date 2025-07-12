cec5956891a4d7cc18195c21b67799ccdec3951d
Romiz Aisy <romizaisy21@gmail.com>
2025-07-12 22:29:41 +0700

    meow

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/public/build/', 
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
@@ -12,7 +12,7 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
-        tailwindcss(),
+
     ],
     esbuild: {
         jsx: 'automatic',
